f417fb345131fe91f831cb52b9246b49ac97fa5c
Maxitosh <maxitoshs@gmail.com>
2021-08-21 20:25:42 +0300

    Moscow time response for / request requirements file

---
+++ app_python/server.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from pytz import timezone
+
+
+def process_http_request(environ, start_response):
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain; charset=utf-8'),
+    ]
+    start_response(status, response_headers)
+    time = str(timezone('Europe/Moscow').localize(datetime.now())).encode('utf-8')
+    return [time]
