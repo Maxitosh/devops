de5bf932a8c2ba6c5de5b1239b318468334e132c
Max Kureykin <maxitoshs@gmail.com>
2021-09-06 20:42:23 +0300

    Add ansible stuff

--- IaC/cloud/aws.tf
+++ IaC/cloud/aws.tf
@@ -12,9 +12,50 @@ terraform {
     }
   }
 }
+
+resource "aws_key_pair" "pubkey" {
+  key_name   = "devops-ssh-key"
+  public_key = file(var.pubkey)
+}
+
+resource "aws_security_group" "app_security_group" {
+  name        = "Application security group"
+  description = "Allow ssh and app ports"
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "python_app"
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
 resource "aws_instance" "vm" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                         = "ami-0c55b159cbfafe1f0"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.pubkey.key_name
+  vpc_security_group_ids      = [aws_security_group.app_security_group.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "lab-3-vm"
